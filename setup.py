cac21a5367c8c19fe507cd0a85e27f471288f228
mamachanko <max@rootswiseyouths.com>
2015-08-24 21:00:19 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    description = f.read()
+
+
+setup(
+    name='release-script-test',
+    version='0.1.0-dev',
+    packages=find_packages(),
+    namespace_packages=['dhh'],
+    description=u'for testing release-script',
+    long_description=description,
+)
